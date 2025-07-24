c5f13fd5a32158e4ec20b58b1ec234d6c746be21
Mrunmai Abhyankar <mrunmaiabhyankar@Mac.lan>
2025-07-24 02:04:52 -0500

    Changes for live region attempt 11

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -76,7 +76,7 @@ const Home: React.FC = () => {
           <button className='secondary-btn' onClick={() => navigate('/tutorial-1')}>Tutorial</button>
           <button className='secondary-btn' onClick={() => logout()}>Logout</button>
         </div>
-        <p>Version v1.7</p>
+        <p>Version v1.8</p>
       </div>
     </div>
   );
