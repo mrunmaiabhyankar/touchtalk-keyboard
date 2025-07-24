0df1e1c71675539e9acfe03d276df61b23685cc9
Mrunmai Abhyankar <mrunmaiabhyankar@Mac.lan>
2025-07-24 01:14:02 -0500

    Text box attempt 4

--- src/components/KeyboardGrid.tsx
+++ src/components/KeyboardGrid.tsx
@@ -97,10 +97,19 @@ const KeyboardGrid: React.FC<KeyboardGridProps> = ({
       default:
         if (variant === "gridLayout" && content === "single") {
           newText = letter;
+          setTimeout(() => {
+          setAriaMessage("");
+          }, 10)
         } else if (variant === "gridLayout" && content === "multiple") {
           newText = ariaLabelData[index];
+          setTimeout(() => {
+          setAriaMessage("");
+          }, 10)
         } else{
           newText += letter; // Default behavior for other variants
+          // setTimeout(() => {
+          // setAriaMessage("");
+          // }, 10)
         }
         // newText = content === "multiple" ? newText + letter : letter;
         break;
